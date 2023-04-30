64c402517bd7d105c3a4723dd657627da685aca6
Madhubhashana99 <104136158+Madhubhashana99@users.noreply.github.com>
2023-04-30 23:52:20 +0530

    updated

--- script.js
+++ script.js
@@ -52,6 +52,7 @@ function startQuiz(){
 }
 
 function showQuestion(){
+  resetState();
   let currentQuestion = questions[currentQuestionIndex];
   let questionNo = currentQuestionIndex + 1;
   questionElement.innerHTML = questionNo + ". "+currentQuestion.
@@ -62,5 +63,27 @@ function showQuestion(){
     button.innerHTML = answer.text;
     button.classList.add("btn");
     answerButton.appendChild(button);
+    if(answer.correct){
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener("click", selectAnswer);
   });
 }
+
+function resetState(){
+  nextButton.style.display = "none";
+  while (answerButtons.firstChild) {
+    answerButtons.removeChild(answerButtons.firstChild);
+  }
+}
+
+function selectAnswer(e){
+  const selectedBtn = e.target;
+  const isCorrect = selectedBtn.dataset.correct === "true";
+  if (isCorrect) {
+    selectedBtn.classList.add("correct");
+  }else{
+    selectedBtn.classList.add("incorrect");
+  }
+}
+startQuiz();
